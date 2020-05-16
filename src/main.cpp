f3b2515417e7f1c45ae81bffd64a6705b7be862f
NukeBird <nukebird.dev@gmail.com>
2020-05-16 19:21:11 +0300

    Setup dependencies

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,15 @@
 #include <iostream>
 
+#include <globjects/globjects.h>
+#include <GLFW/glfw3.h>
+#include <glm/glm.hpp>
+#include <assimp/Importer.hpp>
+#include <SOIL2/SOIL2.h>
+#include <spdlog/spdlog.h>
+
 int main()
 {
+	glfwInit(); //test
 	std::cout << "Oh hi Mark" << std::endl;
 	return EXIT_SUCCESS;
 }
